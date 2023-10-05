5f9df556802ac35ad0d9bbc26ccd7270f9f0e309
SANDEEP KUMAR <143108427+CAP05090@users.noreply.github.com>
2023-10-05 08:02:40 +0530

    Add files via upload

---
+++ Fetch_Scholar/index.js
@@ -0,0 +1,34 @@
+let rootele = document.getElementById("root")
+
+let url = "https://fakestoreapi.com/products"
+
+const scholar = async () => {
+    try{
+        fetch(url)
+        let res = await fetch(url)
+        let data = await res.json()
+        printdata(data)
+    }catch(error){
+        console.log(error)
+    }
+}
+const printdata = (data) => {
+    data.forEach(product => {
+        let productcart = document.createElement("div")
+        product.className = "product_cart"
+
+        let productImg = document.createElement("img")
+        productImg.src = product.image
+
+        let productName = document.createElement("p")
+        productName.textContent = product.title
+
+        let productprice = document.createElement("h3")
+        productprice.textContent = "Price:- " + product.price
+
+        productcart.append(productImg,productprice, productName)
+        rootele.append(productcart)
+    });
+}
+
+scholar()
